c8f2072159c9fbe284f398f31b1090912afdd270
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-07-04 15:05:05 +0200

    Added a form for details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
@@ -15,10 +16,22 @@ export class DetailsComponent implements OnInit {
   route: ActivatedRoute= inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  })
 
   ngOnInit(): void {
     const housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
     this.housingLocation = this.housingService.
     getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
