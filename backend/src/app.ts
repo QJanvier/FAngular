2db7d81c6f6181ea0a62096253b5da3cbd9ebb5f
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-09-10 18:52:39 +0200

    mongoose and connect database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
+mongoose.connect('mongodb://localhost/FAngu')
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log('Error ${err}'));
+
+app.use(express.json())
+
 app.use(routes)
- 
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
+
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
