1edfb25ab07cbda91588c9dd6a4b5d8106bdf6ec
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-09-11 17:20:02 +0200

    install depedencies and set up base session

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const passport = require('passport')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const MongoStore = require('connect-mongo')
 const routes = require('./routes')
 
 mongoose.connect('mongodb://localhost/FAngu')
@@ -8,7 +12,21 @@ mongoose.connect('mongodb://localhost/FAngu')
   .catch((err) => console.log('Error ${err}'));
 
 app.use(express.json())
+app.use(cookieParser("mySecret"))
 
+app.use(session(
+  {
+    secret: "mySecret",
+    saveUnitialized: true,
+    resave: false,
+    cookie: { maxAge: 6000 * 60},
+    store: MongoStore.create({ client: mongoose.connection.getClient() })
+  }
+))
+
+
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(routes)
 
 const PORT = process.env.PORT || 3000;
@@ -16,7 +34,8 @@ app.listen(PORT, console.log(`Server running on port ${PORT}`))
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  req.session.visited = true
+  res.cookie('Pedro', 'express', { maxAge: 6000, signed: true})
+  res.status(201).send({msg: 'Hello World!'})
 })
  
-app.listen(3000)
