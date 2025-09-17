0f79097b9f119c0462f7665dbb60befd2bb30414
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-09-17 17:52:46 +0200

    writing interface for routing

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,23 @@
 const router = require("express").Router();
 const usersRoutes = require("./users");
-
+const app = require('../app')
 router.use(usersRoutes);
 
 module.exports = router;
 
+export interface BaseRoute {
+  path: string;
+  component?: any;
+  children?: BaseRoute[];
+  canActivate?: any[];
+  canDeactivate?: any[];
+  data?: Record<string, any>;
+  redirectTo?: string;
+  pathMatch?: string;
+}
 
-app.get<>('/',  (req, res) => {
+
+router.get<>('/',  (req, res) => {
   req.session.visited = true
   res.cookie('Pedro', 'express', { maxAge: 6000, signed: true})
   res.status(201).send({msg: 'Hello World!'})
